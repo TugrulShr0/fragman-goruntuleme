55e010a2e58e6b7518c2114779eee97bc74b6bf9
TugrulShr0 <tugrulshr0@gmail.com>
2025-04-27 14:54:28 +0300

    Fragman
    C# Application

---
+++ sınema_fragman/sınema_fragman/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınema_fragman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void madmaxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=hEJnMQG9ev8");
+            label1.Text = "Mad Max";
+        }
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.BlueViolet;
+        }
+
+        private void griToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu Proje Tuğrul Şahar tarafından yapıldı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void uzayYolcularıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=8GKyQ1S5594");
+            label1.Text = "Uzay Yolcuları";
+        }
+
+        private void korkuSeansıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=5u5A471adxg");
+            label1.Text = "Korku Seansı";
+        }
+
+        private void aynıYıldızınAltındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.youtube.com/watch?v=aQnlngjZG48");
+            label1.Text = "Aynı Yıldızın Altında";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
